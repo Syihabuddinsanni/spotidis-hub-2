5cdb4b0ea243e3805b457c2a2d59e28479c0ed17
unknown <muhammad_izzul_28rpl@student.smktelkom-mlg.sch.id>
2021-07-18 10:59:18 +0700

    Update #63

--- commands/resume.js
+++ commands/resume.js
@@ -19,8 +19,8 @@ module.exports = {
      */
     run: async (client, message, args, { GuildDB }) => {
         let player = await client.Manager.get(message.guild.id);
-        if (!player) return client.sendTime(message.channel, "<a:warn:866161245232693291> | Tidak ada musik yang sedang diputar.");
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "<a:warn:866161245232693291> | **Masuk voice channel dulu sobad!**");
+        if (!player) return client.sendTime(message.channel, "<a:warn:866161245232693291> **|** Tidak ada musik yang sedang diputar.");
+        if (!message.member.voice.channel) return client.sendTime(message.channel, "<a:warn:866161245232693291> Masuk voice channel dulu sobad!");
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Yah bot nya lagi di pake orang**");
 
         if (player.playing) return client.sendTime(message.channel, "<a:warn:866161245232693291> | **Musik tidak di pause**");
@@ -40,11 +40,11 @@ module.exports = {
             const guild = client.guilds.cache.get(interaction.guild_id);
             const member = guild.members.cache.get(interaction.member.user.id);
 
-            if (!member.voice.channel) return client.sendTime(interaction, "<a:warn:866161245232693291> | **You must be in a voice channel to use this command.**");
+            if (!member.voice.channel) return client.sendTime(interaction, "<a:warn:866161245232693291>  Masuk voice channel dulu sobad!");
             if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Yah bot nya lagi di pake orang**");
 
             let player = await client.Manager.get(interaction.guild_id);
-            if (!player) return client.sendTime(interaction, "<a:warn:866161245232693291> | Tidak ada musik yang sedang diputar.");
+            if (!player) return client.sendTime(interaction, "<a:warn:866161245232693291> **|** Tidak ada musik yang sedang diputar.");
             if (player.playing) return client.sendTime(interaction, "<a:warn:866161245232693291> | **Musik tidak di pause**");
             player.pause(false);
             client.sendTime(interaction, "**⏯ Dilanjutkan!**");
