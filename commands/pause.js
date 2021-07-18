cfd04cd7a1dc0843fabaf5777d87e1975f1e7c80
unknown <muhammad_izzul_28rpl@student.smktelkom-mlg.sch.id>
2021-07-18 10:37:57 +0700

    Update #62

--- commands/pause.js
+++ commands/pause.js
@@ -19,10 +19,10 @@ module.exports = {
      */
     run: async (client, message, args, { GuildDB }) => {
         let player = await client.Manager.get(message.guild.id);
-        if (!player) return client.sendTime(message.channel, "<a:alert1:853103770254180372> | **Tidak ada musik yang diputar**"); //Nothing is playing right now...
-        if (!message.member.voice.channel) return client.sendTime(message.channel, "<a:alert1:853103770254180372> | **Masuk voice channel dulu sobad...**");
+        if (!player) return client.sendTime(message.channel, "<a:warn:866161245232693291> | **Tidak ada musik yang diputar**"); //Nothing is playing right now...
+        if (!message.member.voice.channel) return client.sendTime(message.channel, "<a:warn:866161245232693291> | **Masuk voice channel dulu sobad...**");
         if (message.guild.me.voice.channel && message.member.voice.channel.id !== message.guild.me.voice.channel.id) return client.sendTime(message.channel, ":x: | **Bot nya lagi dipake cuk!**");
-        if (player.paused) return client.sendTime(message.channel, "<a:alert1:853103770254180372> | **Music sudah dihentikan!**"); //Music already paused!
+        if (player.paused) return client.sendTime(message.channel, "<a:warn:866161245232693291> | **Music sudah dihentikan!**"); //Music already paused!
         player.pause(true);
         let embed = new MessageEmbed().setAuthor(`Di pause!`, client.config.IconURL).setColor("RANDOM").setDescription(`Type \`${GuildDB.prefix}resume\` untuk melanjutkan!`);
         await message.channel.send(embed);
@@ -41,11 +41,11 @@ module.exports = {
             const guild = client.guilds.cache.get(interaction.guild_id);
             const member = guild.members.cache.get(interaction.member.user.id);
 
-            if (!member.voice.channel) return client.sendTime(interaction, "<a:alert1:853103770254180372> | **Masuk ke voice channel dulu sobad!**");
+            if (!member.voice.channel) return client.sendTime(interaction, "<a:warn:866161245232693291> | **Masuk ke voice channel dulu sobad!**");
             if (guild.me.voice.channel && !guild.me.voice.channel.equals(member.voice.channel)) return client.sendTime(interaction, ":x: | **Bot nya lagi dipake cuk!**");
 
             let player = await client.Manager.get(interaction.guild_id);
-            if (!player) return client.sendTime(interaction, "<a:alert1:853103770254180372> | **Tidak ada yang sedang diputar...**");
+            if (!player) return client.sendTime(interaction, "<a:warn:866161245232693291> | **Tidak ada yang sedang diputar...**");
             if (player.paused) return client.sendTime(interaction, "Music sudah Di hentikan!");
             player.pause(true);
             client.sendTime(interaction, "**⏸ Di pause!**");
