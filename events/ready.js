8daadd95f9c556b4247657b0bad5f35be71aded9
unknown <muhammad_izzul_28rpl@student.smktelkom-mlg.sch.id>
2021-07-21 22:52:26 +0700

    #12 test new ui

--- events/ready.js
+++ events/ready.js
@@ -2,10 +2,19 @@ module.exports = async (client) => {
   client.Ready = true, 
   client.user.setPresence({
     status: "idle",  // You can show online, idle, and dnd
-    activity: {
-        name: "WORKING ON UPDATE!",  // The message shown
+    activity: [{
+        name: "Working On Update!",  // The message shown
         type: "WATCHING", // PLAYING, WATCHING, LISTENING, STREAMING,
     }
+    , {
+        type: "LISTENING",
+        name: "!help | sadis.gang",
+        url: "https://discord.gg/gj65TB8Y"
+    }] ,
+    //status akan ganti setiap 15 secod
+    interval: 15
+
+
 });
     client.Manager.init(client.user.id);
     client.log("Successfully Logged in as " + client.user.tag); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
